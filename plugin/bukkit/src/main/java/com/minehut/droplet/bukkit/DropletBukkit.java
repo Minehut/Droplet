0c90336d9065dd02c6910be9f5f928dcc7ea49a3
Luke Chatton <chattonluke@gmail.com>
2016-07-03 16:46:33 -0700

    bukkit plugin shell

---
+++ plugin/bukkit/src/main/java/com/minehut/droplet/bukkit/DropletBukkit.java
@@ -0,0 +1,14 @@
+package com.minehut.droplet.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by luke on 7/3/16.
+ */
+public class DropletBukkit extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
