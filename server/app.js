21e43b2dd4461c10971c76f854a453d83b1546af
Luke Chatton <chattonluke@gmail.com>
2016-07-04 09:55:47 -0700

    fix config.json loading

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var config = require('config.json');
+var config = require('./config.json');
 
 var mongoose = require('mongoose');
 mongoose.connect(config.mongo);
